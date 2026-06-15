9fd9d4e17e1d37d57dadb3d758e809807e829fdd
Vanshi-2522 <vanshi22gupta@gmail.com>
2026-06-15 13:40:19 +0530

    [Vanshika] Add: Added UserName Validation Scenario-Based Problem

---
+++ core-java-practice/leet-code-codebase/UsernameValidator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class UsernameValidator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String username = sc.nextLine();
+
+        if (username.contains(" "))
+            System.out.println("The username contains spaces.");
+        else
+            System.out.println("The username does not contain spaces.");
+
+        System.out.println("Total number of characters: " + username.length());
+        System.out.println("Username in uppercase: " + username.toUpperCase());
+
+        String reversed = new StringBuilder(username).reverse().toString();
+
+        if (username.equalsIgnoreCase(reversed))
+            System.out.println("The username is a palindrome.");
+        else
+            System.out.println("The username is not a palindrome.");
+
+        sc.close();
+    }
+}
