e06f77d016b1be8c7bcf9f6a7bd6dbdc4b6625fa
Vanshi-2522 <vanshi22gupta@gmail.com>
2026-06-18 23:27:44 +0530

    [Vanshika] Add: Added Exception Handling assignments

---
+++ core-java-practice/gcr-codebase/ExceptionHandling/NestedTryCatch.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class NestedTryCatch {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter index: ");
+            int index = sc.nextInt();
+
+            try {
+                System.out.print("Enter divisor: ");
+                int divisor = sc.nextInt();
+
+                int result = arr[index] / divisor;
+                System.out.println("Result: " + result);
+
+            } catch (ArithmeticException e) {
+                System.out.println("Cannot divide by zero!");
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid array index!");
+        }
+
+        sc.close();
+    }
+}
